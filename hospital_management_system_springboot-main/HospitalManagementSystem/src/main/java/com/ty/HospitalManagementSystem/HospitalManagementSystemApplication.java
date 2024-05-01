b645d40be120429593a9a89a8f65fc84d2dfbfdb
sakthirangasamy <sakthirangasamy2003@gmail.com>
2024-05-01 07:24:04 +0530

    created

---
+++ hospital_management_system_springboot-main/HospitalManagementSystem/src/main/java/com/ty/HospitalManagementSystem/HospitalManagementSystemApplication.java
@@ -0,0 +1,22 @@
+package com.ty.HospitalManagementSystem;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+
+
+@SpringBootApplication
+public class HospitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+	}
+	
+	
+	
+}
